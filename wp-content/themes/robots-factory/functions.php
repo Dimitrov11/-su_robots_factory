da492a30a673158632d7671cdd338dc5e069e295
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-06-24 17:57:18 +0300

    Initial commit to  add footer, header and functions.php

---
+++ wp-content/themes/robots-factory/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+if ( ! defined( 'ROBOTS_FACTORY_ASSETS_VERSION' ) ) {
+    define('ROBOTS_FACTORY_ASSETS_VERSION', '0.1' );
+}
+
+// Define a global variable to reuse, to get the assets
+if ( ! defined( 'ROBOTS_FACTORY_ASSETS_URL' ) ) {
+    define('ROBOTS_FACTORY_ASSETS_URL', get_template_directory_uri() . '/assets/' );
+}
+
+/** Function that enqueue all of the assets */
+function enqueue_robots_theme_assets() {
+
+    // Define the URL of the theme assets directory 
+    $ROBOTS_FACTORY_ASSETS_URL = get_template_directory_uri() . '/assets/';
+
+    // Theme Google Fonts
+    wp_enqueue_style('font-roboto', 'https://fonts.googleapis.com/css?family=Roboto:400,300,500,700&display=swap', false );
+    wp_enqueue_style('font-oswald', 'https://fonts.googleapis.com/css?family=Oswald:400,300,700', false );
+
+    // Theme Styles
+    wp_enqueue_style('bootstrap', $ROBOTS_FACTORY_ASSETS_URL . 'css/bootstrap.min.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_style('magnific-popup', $ROBOTS_FACTORY_ASSETS_URL . 'css/magnific-popup.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_style('owl-carousel', $ROBOTS_FACTORY_ASSETS_URL . 'css/owl.carousel.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_style('owl-carousel-theme-min', $ROBOTS_FACTORY_ASSETS_URL . 'css/owl.carousel.theme.min.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_style('ionicons', $ROBOTS_FACTORY_ASSETS_URL . 'css/ionicons.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_style('main', $ROBOTS_FACTORY_ASSETS_URL . 'css/main.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
+    
+    // Theme Scripts
+    // wp_enqueue_script('jquery', $ROBOTS_FACTORY_ASSETS_URL . 'js/jquery-1.12.3.min.js', array() );
+    wp_enqueue_script('bootstrap', $ROBOTS_FACTORY_ASSETS_URL . 'js/bootstrap.min.js', array( 'jquery' ), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_script('jquery.magnific-popup', $ROBOTS_FACTORY_ASSETS_URL . 'js/jquery.magnific-popup.min.js', array( 'jquery' ), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_script('owl.carousel', $ROBOTS_FACTORY_ASSETS_URL . 'js/owl.carousel.min.js', array( 'jquery' ), ROBOTS_FACTORY_ASSETS_VERSION );
+    wp_enqueue_script('script', $ROBOTS_FACTORY_ASSETS_URL . 'js/script.js', array( 'jquery' ), ROBOTS_FACTORY_ASSETS_VERSION );
+}
+add_action('wp_enqueue_scripts', 'enqueue_robots_theme_assets');
