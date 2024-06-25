3aa353a340f06dc4f7887015a8152853d56345c3
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-06-25 17:48:11 +0300

    Initial commit for adding post and page view

--- wp-content/themes/robots-factory/functions.php
+++ wp-content/themes/robots-factory/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// Define a global for the version
 if ( ! defined( 'ROBOTS_FACTORY_ASSETS_VERSION' ) ) {
     define('ROBOTS_FACTORY_ASSETS_VERSION', '0.1' );
 }
@@ -9,6 +10,9 @@ if ( ! defined( 'ROBOTS_FACTORY_ASSETS_URL' ) ) {
     define('ROBOTS_FACTORY_ASSETS_URL', get_template_directory_uri() . '/assets/' );
 }
 
+// Add theme support title-tag
+add_theme_support( 'title-tag' );
+
 /** Function that enqueue all of the assets */
 function enqueue_robots_theme_assets() {
 
@@ -16,8 +20,8 @@ function enqueue_robots_theme_assets() {
     $ROBOTS_FACTORY_ASSETS_URL = get_template_directory_uri() . '/assets/';
 
     // Theme Google Fonts
-    wp_enqueue_style('font-roboto', 'https://fonts.googleapis.com/css?family=Roboto:400,300,500,700&display=swap', false );
-    wp_enqueue_style('font-oswald', 'https://fonts.googleapis.com/css?family=Oswald:400,300,700', false );
+    wp_enqueue_style('font-roboto', 'https://fonts.googleapis.com/css?family=Roboto:400,300,500,700&display=swap' );
+    wp_enqueue_style('font-oswald', 'https://fonts.googleapis.com/css?family=Oswald:400,300,700' );
 
     // Theme Styles
     wp_enqueue_style('bootstrap', $ROBOTS_FACTORY_ASSETS_URL . 'css/bootstrap.min.css', array(), ROBOTS_FACTORY_ASSETS_VERSION );
@@ -35,3 +39,13 @@ function enqueue_robots_theme_assets() {
     wp_enqueue_script('script', $ROBOTS_FACTORY_ASSETS_URL . 'js/script.js', array( 'jquery' ), ROBOTS_FACTORY_ASSETS_VERSION );
 }
 add_action('wp_enqueue_scripts', 'enqueue_robots_theme_assets');
+
+
+/** Register Navigation Menu */
+function robots_register_nav_menus() {
+    register_nav_menus( array(
+            'header-menu' => __( 'Header Menu' ),
+        )
+    );
+}
+add_action( 'init', 'robots_register_nav_menus' );
